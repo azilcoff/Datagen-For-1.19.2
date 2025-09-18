4fb00ba8053c2121533b024ec7356c0cea7d4bbc
Aziz <128413744+azilcoff@users.noreply.github.com>
2025-09-18 22:43:10 +0300

    Update ModBlockStateProvider.java

--- ModBlockStateProvider.java
+++ ModBlockStateProvider.java
@@ -2,23 +2,26 @@ package net.azilcoff.azizmod.datagen;
 
 import net.azilcoff.azizmod.AzizMod;
 import net.azilcoff.azizmod.block.ModBlocks;
+import net.azilcoff.azizmod.tag.ModTags;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.Nullable;
 
-public class ModBlockStateProvider extends BlockStateProvider {
-    public ModBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
-        super(gen, AzizMod.MOD_ID, exFileHelper);
+public class ModBlockTagProvider extends BlockTagsProvider {
+    public ModBlockTagProvider(DataGenerator p_126511_, @Nullable ExistingFileHelper existingFileHelper) {
+        super(p_126511_, AzizMod.MOD_ID, existingFileHelper);
     }
 
     @Override
-    protected void registerStatesAndModels() {
-        blockWithItem(ModBlocks.RUDIUM_ORE);
-    }
+    protected void addTags() {
+        tag(ModTags.Block.RUDIUM_ORES).add(ModBlocks.RUDIUM_ORE.get());
 
-    private void blockWithItem (RegistryObject<Block> blockRegistryObject){
-        simpleBlockItem(blockRegistryObject.get(),cubeAll(blockRegistryObject.get()));
+        tag(BlockTags.NEEDS_IRON_TOOL).addTag(ModTags.Block.RUDIUM_ORES);
+        tag(BlockTags.MINEABLE_WITH_PICKAXE).addTag(ModTags.Block.RUDIUM_ORES);
+
+        super.addTags();
     }
 }
+
